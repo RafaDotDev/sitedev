0a4b3ded0052b190f9efc817b40f110152089c48
Rafael Santos <rafael-rsantos@hotmail.com>
2021-02-11 22:10:06 -0300

    added Rafa before the phrase

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to the jungle!!</h1>
+        <h1 className={styles.title}>Rafael, welcome to the jungle!!</h1>
       </main>
     </div>
   );
